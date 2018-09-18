b0fd4865ca97e0dff840f9feb5e9f97108d414d1
Bernardo Henrique <bernardohrl.unb@gmail.com>
2018-09-18 05:33:57 -0300

    Initial Commit
    Initial commit with project base.
    Already dockerized.
    Project's test setup configured.

---
+++ project/config.py
@@ -0,0 +1,12 @@
+class	BaseConfig:
+    TESTING	=	False
+
+class	DevelopmentConfig(BaseConfig):
+    THIS = 'Development'
+
+class	TestingConfig(BaseConfig):
+    TESTING	=	True
+    THIS = 'Testing'
+
+class	ProductionConfig(BaseConfig):
+    THIS = 'Production' 
